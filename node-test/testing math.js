8dc6227620bfa8cc031698b0e5bede8b5762c0ae
joojooring <ghkdwnfl0557@naver.com>
2023-10-10 18:40:22 +0900

    nodejs+

---
+++ node-test/testing math.js
@@ -0,0 +1,20 @@
+const add = (a,b) =>a+b;
+const minus = (a,b) =>a-b;
+const PI = 3.14;
+
+
+// module.exports = {
+//     add,
+//     minus,
+//     PI
+// };
+
+module.exports.add = (a,b) => a+b;
+module.exports.minus = (a,b) =>a-b;
+module.exports.PI = 3.14;
+
+
+// key : "value";
+// name: "lily";
+// gender : "female";
+

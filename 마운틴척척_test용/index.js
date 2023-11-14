8752130007953b670fd3bd45e8c99afac84df086
joojooring <ghkdwnfl0557@naver.com>
2023-11-14 13:06:34 +0900

    maptest_click

--- 마운틴척척_test용/index.js
+++ 마운틴척척_test용/index.js
@@ -11,7 +11,7 @@ app.use( express.json() );
 // app.use('/user', router);
 
 app.get('/', (req,res) =>{
-    res.render("marker_clickevent");
+    res.render("소희님_map완료");
 });
 
 app.get('*', (req,res) =>{

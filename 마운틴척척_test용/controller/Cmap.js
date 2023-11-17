1c76462129c95ef4b87f2d8187881b7168ea1e25
joojooring <ghkdwnfl0557@naver.com>
2023-11-17 21:30:19 +0900

    지도 갤러리 imgurl수정중

--- 마운틴척척_test용/controller/Cmap.js
+++ 마운틴척척_test용/controller/Cmap.js
@@ -1,11 +1,25 @@
-const { Gallery } = require('../model')
+const { Gallery } = require("../model");
 
+// 지도 화면 UI
+exports.mapUiPage = (req, res) => {
+  res.render("map");
+};
+
+// 지도 api 이용 페이지
 exports.mapPage = (req, res) => {
-  res.render("map완료_if_heart_true수정중2");
+  res.render("map_api_complete");
 };
 
 exports.getMap = (req, res) => {
-    Gallery.findAll().then((result) => {
-      res.send(result)
-    })
-  }
+  Gallery.findAll().then((result) => {
+    res.send(result);
+  });
+};
+
+
+// 추가
+exports.postInfo = (req, res) => {
+  Gallery.findAll().then((result) => {
+    res.send(result);
+  });
+}
--- 마운틴척척_test용/routes/index.js
+++ 마운틴척척_test용/routes/index.js
@@ -2,68 +2,60 @@ const express = require("express");
 const mainRouter = express.Router();
 const userRouter = express.Router();
 const galleryRouter = express.Router();
-const mapRouter = express.Router();
-const commentRouter = express.Router();
-
-
 
 // 메인페이지 관련
 const main = require("../controller/Cmain");
 mainRouter.get("/", main.mainPage);
-
-// 지도관련
-const map = require("../controller/Cmap");
-mapRouter.get("/maptest", map.mapPage);
-mapRouter.get("/mapexhibition", map.getMap);
-
-
-// mapPage
 
 // 로그인 관련
 const signin = require("../controller/Csignin");
 userRouter.get("/signin", signin.loginPage);
 userRouter.post("/signin", signin.postLogin);
 
+// 로그아웃 관련
+const logout = require("../controller/Cmain");
+mainRouter.post("/logout", main.logOut);
+
 // 회원가입 관련
 const signup = require("../controller/Csignup");
 userRouter.get("/signup", signup.signUpPage);
+userRouter.post("/signupcheck", signup.postSignUpCheck);
 userRouter.post("/signup", signup.postSignUp);
-userRouter.post("/signup", signup.postSignUpCheck);
 
-// 마이페이지(회원정보 수정) 관련
+// 마이페이지(회원정보 수정/탈퇴) 관련
 const profile = require("../controller/Cprofile");
 userRouter.get("/profile", profile.selectProfilePage);
 userRouter.get("/profile/editcheck", profile.editCheckPage);
+userRouter.post("/profile/editcheck", profile.editCheckPw);
 userRouter.get("/profile/edit", profile.profilePage);
 userRouter.patch("/profile/edit/:u_id", profile.profileEdit);
+userRouter.patch("/profile/editall/:u_id", profile.profileAllEdit);
 userRouter.get("/profile/delete", profile.deleteCheckPage);
-userRouter.delete("/profile/delete/:u_id", profile.profileDelete);
+userRouter.post("/profile/delete", profile.userDelete);
+
+// 지도관련
+const map = require("../controller/Cmap");
+galleryRouter.get("/map", map.mapUiPage);
+galleryRouter.get("/maptest", map.mapPage);
+galleryRouter.get("/mapexhibition", map.getMap);
+
+// 추가
+galleryRouter.post("/info", map.postInfo);
+
+
+
 
 // 카테고리 관련
 const category = require("../controller/Clist");
-galleryRouter.get("/category", category.categoryPage);
+galleryRouter.get("/category", category.categoryAllPage);
+galleryRouter.get("/category/exhibition", category.categoryExhibition);
+galleryRouter.get("/category/artgallery", category.categoryArtgallery);
+galleryRouter.get("/category/museum", category.categoryMuseum);
+galleryRouter.get("/category/popupstore", category.categoryPopup);
 
-
-
-// comment 관련
-
-commentRouter.get("/commenthome", controller.home);
-
-commentRouter.get("/comment", controller.comment);
-// 방명록 등록
-commentRouter.post("/comment", controller.postComment);
-// 방명록 수정
-commentRouter.patch("/comment", controller.patchComment);
-
-// 방명록 하나 조회
-commentRouter.get("/comment/:id", controller.getCommentById);
-// 방명록 삭제
-commentRouter.delete("/comment/:id", controller.deleteComment);
 
 module.exports = {
   mainRouter,
   userRouter,
   galleryRouter,
-  mapRouter,
-  commentRouter
 };
